b431be32ef9cae73a4f9a0528679a639368d8e88
OscarCrs <zcrscortez@gmail.com>
2026-07-13 12:16:05 -0600

    Añadir formulario de inicio de sesión a la aplicación
    Se agregó el formulario `LoginForm` al proyecto, incluyendo:
    - Archivo `LoginForm.cs` con la lógica principal del formulario.
    - Archivo `LoginForm.Designer.cs` con el diseño visual del formulario.
    - Actualización de `EduCafeProyecto.csproj` para incluir los nuevos archivos.
    - Actualización de `EduCafeProyecto.slnx` con un identificador único para el proyecto.
    
    El formulario incluye:
    - Panel izquierdo con logo, título y subtítulo.
    - Panel derecho con campos de usuario y contraseña, checkbox para mostrar contraseña, y botones para iniciar sesión o salir.
    - Estilos personalizados para mejorar la apariencia visual.
    - Configuración de tamaño fijo, posición centrada y título de la ventana.

---
+++ 1-login/LoginForm.Designer.cs
@@ -0,0 +1,275 @@
+﻿namespace EduCafeProyecto._1_login
+{
+    partial class LoginForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panelIzquierdo = new System.Windows.Forms.Panel();
+            this.lblSubtitulo = new System.Windows.Forms.Label();
+            this.lblTituloApp = new System.Windows.Forms.Label();
+            this.picLogo = new System.Windows.Forms.PictureBox();
+            this.panelDerecho = new System.Windows.Forms.Panel();
+            this.btnSalir = new System.Windows.Forms.Button();
+            this.panelSeparador = new System.Windows.Forms.Panel();
+            this.lblO = new System.Windows.Forms.Label();
+            this.btnIniciarSesion = new System.Windows.Forms.Button();
+            this.chkMostrarContrasena = new System.Windows.Forms.CheckBox();
+            this.txtContrasena = new System.Windows.Forms.TextBox();
+            this.lblContrasena = new System.Windows.Forms.Label();
+            this.txtUsuario = new System.Windows.Forms.TextBox();
+            this.lblUsuario = new System.Windows.Forms.Label();
+            this.lblSubBienvenido = new System.Windows.Forms.Label();
+            this.lblBienvenido = new System.Windows.Forms.Label();
+            this.panelIzquierdo.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.picLogo)).BeginInit();
+            this.panelDerecho.SuspendLayout();
+            this.panelSeparador.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panelIzquierdo
+            // 
+            this.panelIzquierdo.BackColor = System.Drawing.Color.FromArgb(16, 48, 33);
+            this.panelIzquierdo.Controls.Add(this.lblSubtitulo);
+            this.panelIzquierdo.Controls.Add(this.lblTituloApp);
+            this.panelIzquierdo.Controls.Add(this.picLogo);
+            this.panelIzquierdo.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panelIzquierdo.Location = new System.Drawing.Point(0, 0);
+            this.panelIzquierdo.Name = "panelIzquierdo";
+            this.panelIzquierdo.Size = new System.Drawing.Size(500, 752);
+            this.panelIzquierdo.TabIndex = 0;
+            // 
+            // lblSubtitulo
+            // 
+            this.lblSubtitulo.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular);
+            this.lblSubtitulo.ForeColor = System.Drawing.Color.DarkGray;
+            this.lblSubtitulo.Location = new System.Drawing.Point(12, 450);
+            this.lblSubtitulo.Name = "lblSubtitulo";
+            this.lblSubtitulo.Size = new System.Drawing.Size(476, 30);
+            this.lblSubtitulo.TabIndex = 2;
+            this.lblSubtitulo.Text = "SISTEMA DE CONTROL DE VENTAS";
+            this.lblSubtitulo.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblTituloApp
+            // 
+            this.lblTituloApp.Font = new System.Drawing.Font("Segoe UI", 28F, System.Drawing.FontStyle.Bold);
+            this.lblTituloApp.ForeColor = System.Drawing.Color.White;
+            this.lblTituloApp.Location = new System.Drawing.Point(12, 380);
+            this.lblTituloApp.Name = "lblTituloApp";
+            this.lblTituloApp.Size = new System.Drawing.Size(476, 60);
+            this.lblTituloApp.TabIndex = 1;
+            this.lblTituloApp.Text = "Edu Café";
+            this.lblTituloApp.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // picLogo
+            // 
+            this.picLogo.Location = new System.Drawing.Point(125, 110);
+            this.picLogo.Name = "picLogo";
+            this.picLogo.Size = new System.Drawing.Size(250, 250);
+            this.picLogo.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.picLogo.TabIndex = 0;
+            this.picLogo.TabStop = false;
+            // 
+            // panelDerecho
+            // 
+            this.panelDerecho.BackColor = System.Drawing.Color.FromArgb(245, 245, 245);
+            this.panelDerecho.Controls.Add(this.btnSalir);
+            this.panelDerecho.Controls.Add(this.panelSeparador);
+            this.panelDerecho.Controls.Add(this.btnIniciarSesion);
+            this.panelDerecho.Controls.Add(this.chkMostrarContrasena);
+            this.panelDerecho.Controls.Add(this.txtContrasena);
+            this.panelDerecho.Controls.Add(this.lblContrasena);
+            this.panelDerecho.Controls.Add(this.txtUsuario);
+            this.panelDerecho.Controls.Add(this.lblUsuario);
+            this.panelDerecho.Controls.Add(this.lblSubBienvenido);
+            this.panelDerecho.Controls.Add(this.lblBienvenido);
+            this.panelDerecho.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panelDerecho.Location = new System.Drawing.Point(500, 0);
+            this.panelDerecho.Name = "panelDerecho";
+            this.panelDerecho.Size = new System.Drawing.Size(1176, 752);
+            this.panelDerecho.TabIndex = 1;
+            // 
+            // btnSalir
+            // 
+            this.btnSalir.BackColor = System.Drawing.Color.FromArgb(215, 204, 200);
+            this.btnSalir.FlatAppearance.BorderSize = 0;
+            this.btnSalir.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnSalir.Font = new System.Drawing.Font("Segoe UI", 14F, System.Drawing.FontStyle.Regular);
+            this.btnSalir.ForeColor = System.Drawing.Color.Black;
+            this.btnSalir.Location = new System.Drawing.Point(338, 600);
+            this.btnSalir.Name = "btnSalir";
+            this.btnSalir.Size = new System.Drawing.Size(500, 50);
+            this.btnSalir.TabIndex = 9;
+            this.btnSalir.Text = "Salir";
+            this.btnSalir.UseVisualStyleBackColor = false;
+            // 
+            // panelSeparador
+            // 
+            this.panelSeparador.BackColor = System.Drawing.Color.DarkGray;
+            this.panelSeparador.Controls.Add(this.lblO);
+            this.panelSeparador.Location = new System.Drawing.Point(338, 555);
+            this.panelSeparador.Name = "panelSeparador";
+            this.panelSeparador.Size = new System.Drawing.Size(500, 2);
+            this.panelSeparador.TabIndex = 8;
+            // 
+            // lblO
+            // 
+            this.lblO.AutoSize = true;
+            this.lblO.BackColor = System.Drawing.Color.FromArgb(245, 245, 245);
+            this.lblO.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Regular);
+            this.lblO.ForeColor = System.Drawing.Color.Gray;
+            this.lblO.Location = new System.Drawing.Point(240, -8);
+            this.lblO.Name = "lblO";
+            this.lblO.Size = new System.Drawing.Size(17, 19);
+            this.lblO.TabIndex = 0;
+            this.lblO.Text = "o";
+            // 
+            // btnIniciarSesion
+            // 
+            this.btnIniciarSesion.BackColor = System.Drawing.Color.FromArgb(16, 48, 33);
+            this.btnIniciarSesion.FlatAppearance.BorderSize = 0;
+            this.btnIniciarSesion.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnIniciarSesion.Font = new System.Drawing.Font("Segoe UI", 14F, System.Drawing.FontStyle.Bold);
+            this.btnIniciarSesion.ForeColor = System.Drawing.Color.White;
+            this.btnIniciarSesion.Location = new System.Drawing.Point(338, 480);
+            this.btnIniciarSesion.Name = "btnIniciarSesion";
+            this.btnIniciarSesion.Size = new System.Drawing.Size(500, 50);
+            this.btnIniciarSesion.TabIndex = 7;
+            this.btnIniciarSesion.Text = "Iniciar sesión";
+            this.btnIniciarSesion.UseVisualStyleBackColor = false;
+            // 
+            // chkMostrarContrasena
+            // 
+            this.chkMostrarContrasena.AutoSize = true;
+            this.chkMostrarContrasena.Font = new System.Drawing.Font("Segoe UI", 11F, System.Drawing.FontStyle.Regular);
+            this.chkMostrarContrasena.Location = new System.Drawing.Point(338, 430);
+            this.chkMostrarContrasena.Name = "chkMostrarContrasena";
+            this.chkMostrarContrasena.Size = new System.Drawing.Size(154, 24);
+            this.chkMostrarContrasena.TabIndex = 6;
+            this.chkMostrarContrasena.Text = "Mostrar contraseña";
+            this.chkMostrarContrasena.UseVisualStyleBackColor = true;
+            // 
+            // txtContrasena
+            // 
+            this.txtContrasena.Font = new System.Drawing.Font("Segoe UI", 14F, System.Drawing.FontStyle.Regular);
+            this.txtContrasena.Location = new System.Drawing.Point(338, 375);
+            this.txtContrasena.Name = "txtContrasena";
+            this.txtContrasena.Size = new System.Drawing.Size(500, 32);
+            this.txtContrasena.TabIndex = 5;
+            this.txtContrasena.UseSystemPasswordChar = true;
+            // 
+            // lblContrasena
+            // 
+            this.lblContrasena.AutoSize = true;
+            this.lblContrasena.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular);
+            this.lblContrasena.Location = new System.Drawing.Point(334, 345);
+            this.lblContrasena.Name = "lblContrasena";
+            this.lblContrasena.Size = new System.Drawing.Size(89, 21);
+            this.lblContrasena.TabIndex = 4;
+            this.lblContrasena.Text = "Contraseña";
+            // 
+            // txtUsuario
+            // 
+            this.txtUsuario.Font = new System.Drawing.Font("Segoe UI", 14F, System.Drawing.FontStyle.Regular);
+            this.txtUsuario.Location = new System.Drawing.Point(338, 275);
+            this.txtUsuario.Name = "txtUsuario";
+            this.txtUsuario.Size = new System.Drawing.Size(500, 32);
+            this.txtUsuario.TabIndex = 3;
+            // 
+            // lblUsuario
+            // 
+            this.lblUsuario.AutoSize = true;
+            this.lblUsuario.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular);
+            this.lblUsuario.Location = new System.Drawing.Point(334, 245);
+            this.lblUsuario.Name = "lblUsuario";
+            this.lblUsuario.Size = new System.Drawing.Size(64, 21);
+            this.lblUsuario.TabIndex = 2;
+            this.lblUsuario.Text = "Usuario";
+            // 
+            // lblSubBienvenido
+            // 
+            this.lblSubBienvenido.Font = new System.Drawing.Font("Segoe UI", 14F, System.Drawing.FontStyle.Regular);
+            this.lblSubBienvenido.ForeColor = System.Drawing.Color.Gray;
+            this.lblSubBienvenido.Location = new System.Drawing.Point(6, 170);
+            this.lblSubBienvenido.Name = "lblSubBienvenido";
+            this.lblSubBienvenido.Size = new System.Drawing.Size(1158, 30);
+            this.lblSubBienvenido.TabIndex = 1;
+            this.lblSubBienvenido.Text = "Iniciar sesión para continuar";
+            this.lblSubBienvenido.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblBienvenido
+            // 
+            this.lblBienvenido.Font = new System.Drawing.Font("Segoe UI", 36F, System.Drawing.FontStyle.Bold);
+            this.lblBienvenido.Location = new System.Drawing.Point(6, 95);
+            this.lblBienvenido.Name = "lblBienvenido";
+            this.lblBienvenido.Size = new System.Drawing.Size(1158, 70);
+            this.lblBienvenido.TabIndex = 0;
+            this.lblBienvenido.Text = "Bienvenido";
+            this.lblBienvenido.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // LoginForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1676, 752);
+            this.Controls.Add(this.panelDerecho);
+            this.Controls.Add(this.panelIzquierdo);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(1692, 791);
+            this.MinimumSize = new System.Drawing.Size(1692, 791);
+            this.Name = "LoginForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Window Name";
+            this.panelIzquierdo.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.picLogo)).EndInit();
+            this.panelDerecho.ResumeLayout(false);
+            this.panelDerecho.PerformLayout();
+            this.panelSeparador.ResumeLayout(false);
+            this.panelSeparador.PerformLayout();
+            this.ResumeLayout(false);
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panelIzquierdo;
+        private System.Windows.Forms.PictureBox picLogo;
+        private System.Windows.Forms.Label lblSubtitulo;
+        private System.Windows.Forms.Label lblTituloApp;
+        private System.Windows.Forms.Panel panelDerecho;
+        private System.Windows.Forms.Label lblBienvenido;
+        private System.Windows.Forms.Label lblSubBienvenido;
+        private System.Windows.Forms.TextBox txtUsuario;
+        private System.Windows.Forms.Label lblUsuario;
+        private System.Windows.Forms.TextBox txtContrasena;
+        private System.Windows.Forms.Label lblContrasena;
+        private System.Windows.Forms.CheckBox chkMostrarContrasena;
+        private System.Windows.Forms.Button btnIniciarSesion;
+        private System.Windows.Forms.Panel panelSeparador;
+        private System.Windows.Forms.Label lblO;
+        private System.Windows.Forms.Button btnSalir;
+    }
+}
